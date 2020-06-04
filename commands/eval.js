007ab317ce5f50ffb349898864c00b67270e658f
Flolo <sebbythegodkid@gmail.com>
2020-06-03 23:53:10 -0400

    Add eval (finally

---
+++ commands/eval.js
@@ -0,0 +1,28 @@
+module.exports = function(Message, Client) {
+	if (msg.author.id != 299708692129906692) {
+      msg.channel.send(emojis.dnd + " Oops! Only the bot's creator can use this command (for safety purposes)");
+      return;
+    }
+    let cmd = args.splice(1).join(" ").replace("--silent", "");
+    try {
+		let out = eval(";" + cmd);
+		if (msg.content.match(/--silent/)){
+			return;
+		}
+		msg.channel.send({
+        	embed: {
+          		color: 3553599,
+          		title: "JavaScript Evaluated",
+          		description: "```css\nNo problems while running the code\n```\n*Output:*\n```cs\n# " + out + "\n```"
+			}
+      }).catch();
+	} catch(output){
+		msg.channel.send({
+        	embed: {
+          		color: 3553599,
+          		title: "JavaScript Evaluation Error",
+          		description: "```http\n" + output + "\n```"
+			}
+      	}).catch();
+	}
+}
